261140cde8a33a812fe2c2b5340c4e4335f8f892
DonnyDev <jomny11@googlemail.com>
2024-02-20 16:06:43 +0100

    Added gitsigns and which-key.

--- lua/jonathan/plugins/init.lua
+++ lua/jonathan/plugins/init.lua
@@ -1,4 +1,8 @@
 -- return {"add plugins that don't need additional configuration"}
 return {
+  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+
+  "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
 }
